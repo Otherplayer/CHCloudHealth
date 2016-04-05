6e8dddad67208e8aaa29110ff10ce17869f0bda1
Fqah <osmk@qq.com>
2016-04-05 13:48:48 +0800

    f

--- CHCloudHealth/Sections/Login/CHRegisterController.m
+++ CHCloudHealth/Sections/Login/CHRegisterController.m
@@ -8,6 +8,19 @@
 
 #import "CHRegisterController.h"
 
+@interface CHRegisterController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *mobileTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *captchaTextField;
+@property (weak, nonatomic) IBOutlet UIButton *btnGetCaptcha;
+@property (weak, nonatomic) IBOutlet UIButton *btnRegister;
+
+
+
+
+@end
+
 @implementation CHRegisterController
 
 
@@ -16,6 +29,30 @@
     [super viewDidLoad];
     
     [self addBackButton];
+    
+    
+    
+    UIImageView *holder = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 15, 45)];
+    
+    
+    
+    self.mobileTextField.leftViewMode = UITextFieldViewModeAlways;
+    self.mobileTextField.leftView = holder;
+    self.mobileTextField.layer.borderColor = [UIColor color_f2f2f2].CGColor;
+    self.mobileTextField.layer.borderWidth = 1;
+    
+    self.passwordTextField.leftViewMode = UITextFieldViewModeAlways;
+    self.passwordTextField.leftView = holder;
+    self.passwordTextField.layer.borderColor = [UIColor color_f2f2f2].CGColor;
+    self.passwordTextField.layer.borderWidth = 1;
+    
+    self.captchaTextField.leftViewMode = UITextFieldViewModeAlways;
+    self.captchaTextField.leftView = holder;
+    self.captchaTextField.layer.borderColor = [UIColor color_f2f2f2].CGColor;
+    self.captchaTextField.layer.borderWidth = 1;
+    
+    
+    
     
     
 }
