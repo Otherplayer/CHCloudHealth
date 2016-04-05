a53c1a5438f9bd1692f6621968adb14078df012d
otherplayer <osmk@qq.com>
2016-04-05 22:40:52 +0800

    f

--- CHCloudHealth/Sections/Main/View/CHMainStateCell.m
+++ CHCloudHealth/Sections/Main/View/CHMainStateCell.m
@@ -25,15 +25,54 @@
 //0003血糖
 //0004位置
 //0005健康
+//typeCode
 
 - (void)configure:(NSDictionary *)info{
     NSString *title = info[@"name"];
     NSString *date = [NSDate dateFromStr:[NSString stringWithFormat:@"%@",info[@"createDate"]]];
     NSString *state = [NSString stringWithFormat:@"%@",info[@"state"]];
+    NSString *value = [NSString stringWithFormat:@"%@ %@",info[@"val"],[self unitWithType:info[@"typeCode"]]];
     [self.labTitle setText:title];
     [self.labState setText:state];
     [self.labDate setText:date];
+    [self.labValue setAttributedText:[self fixColorText:value]];
+    [self.labValue setAdjustsFontSizeToFitWidth:YES];
+}
+
+
+
+- (NSString *)unitWithType:(NSString *)type{
+    switch (type.integerValue) {
+            case 1:
+            return @"次/分钟";
+            break;
+            case 2:
+            return @"mmHg";
+            break;
+            case 3:
+            return @"mmol/L";
+            break;
+//            case 4:
+//            return @"";
+//            break;
+//            case 5:
+//            return @"";
+//            break;
+        default:
+            return @"error";
+            break;
+    }
+}
+
+- (NSAttributedString *)fixColorText:(NSString *)value{
     
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:value];
+    NSRange valueRange = NSMakeRange(0, [value rangeOfString:@" "].location);
+    NSRange unitRange = NSMakeRange(valueRange.length, value.length - valueRange.length);
+    [attributedString setAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:50]} range:valueRange];
+    [attributedString setAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:17]} range:unitRange];
+    
+    return attributedString;
 }
 
 
