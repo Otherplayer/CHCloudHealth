bcca7e551163c0ce044615117458324170364be9
otherplayer <osmk@qq.com>
2016-04-05 22:53:26 +0800

    f

---
+++ CHCloudHealth/Sections/Main/CHStatisticsController.h
@@ -0,0 +1,13 @@
+//
+//  CHStatisticsController.h
+//  CHCloudHealth
+//
+//  Created by __无邪_ on 16/4/5.
+//  Copyright © 2016年 fqah. All rights reserved.
+//
+
+#import "CHRootViewController.h"
+
+@interface CHStatisticsController : CHRootViewController
+
+@end
---
+++ CHCloudHealth/Sections/Main/CHStatisticsController.m
@@ -0,0 +1,27 @@
+//
+//  CHStatisticsController.m
+//  CHCloudHealth
+//
+//  Created by __无邪_ on 16/4/5.
+//  Copyright © 2016年 fqah. All rights reserved.
+//
+
+#import "CHStatisticsController.h"
+
+@interface CHStatisticsController ()
+
+@end
+
+@implementation CHStatisticsController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
