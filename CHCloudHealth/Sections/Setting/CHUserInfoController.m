40f12c309ad3b57f27f2c75f2d7e6ad1f4cb1661
Fqah <osmk@qq.com>
2016-04-05 18:38:52 +0800

    f

--- CHCloudHealth/Sections/Setting/CHUserInfoController.m
+++ CHCloudHealth/Sections/Setting/CHUserInfoController.m
@@ -21,10 +21,10 @@
     [super viewDidLoad];
     [self addBackButton];
     
-    self.datas = @[@[@{@"title":@"头像",@"detail":[CHUser sharedInstance].avatarurl}],
-                   @[@{@"title":@"昵称",@"detail":[CHUser sharedInstance].name},
+    self.datas = @[@[@{@"title":@"头像",@"detail":[CHUser sharedInstance].avatarurl ? [CHUser sharedInstance].avatarurl : @""}],
+                   @[@{@"title":@"昵称",@"detail":[CHUser sharedInstance].name ? [CHUser sharedInstance].name : @""},
                      @{@"title":@"性别",@"detail":@""},
-                     @{@"title":@"手机号",@"detail":[CHUser sharedInstance].phoneNumber}]];
+                     @{@"title":@"手机号",@"detail":[CHUser sharedInstance].phoneNumber ? [CHUser sharedInstance].phoneNumber : @""}]];
     
 }
 
@@ -60,6 +60,20 @@
     CHUnitCell *cell = [tableView dequeueReusableCellWithIdentifier:identifierUserinfoBody forIndexPath:indexPath];
     [cell setTitle:title detail:detail];
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        
+    }else{
+        if (indexPath.row == 0) {
+            
+        }else if (indexPath.row == 1){
+            
+        }else if (indexPath.row == 2){
+            
+        }
+    }
 }
 
 
