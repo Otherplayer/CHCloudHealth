ec8e9f0e9e13295f0dd8a58dac94bc3b0723ce95
otherplayer <osmk@qq.com>
2016-04-07 22:19:16 +0800

    f

---
+++ CHCloudHealth/Sections/Setting/View/CHMonitorCell.h
@@ -0,0 +1,13 @@
+//
+//  CHMonitorCell.h
+//  CHCloudHealth
+//
+//  Created by __无邪_ on 16/4/7.
+//  Copyright © 2016年 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHMonitorCell : UITableViewCell
+
+@end
---
+++ CHCloudHealth/Sections/Setting/View/CHSwitchCell.m
@@ -0,0 +1,13 @@
+//
+//  CHSwitchCell.m
+//  CHCloudHealth
+//
+//  Created by __无邪_ on 16/4/7.
+//  Copyright © 2016年 fqah. All rights reserved.
+//
+
+#import "CHSwitchCell.h"
+
+@implementation CHSwitchCell
+
+@end
